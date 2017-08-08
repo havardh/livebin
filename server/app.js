5ef8f2f4ec1914ac6bbfb8686e10a96429323a36
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2017-08-08 22:25:34 +0200

    add mongo database

--- server/app.js
+++ server/app.js
@@ -6,11 +6,22 @@ import {type} from "ot-text";
 
 import WebSocket from "ws";
 import WebSocketJSONStream from "websocket-json-stream";
+import ShareDBMongo from "sharedb-mongo";
+
+const username = process.env.MONGO_USERNAME || "";
+const password = process.env.MONGO_PASSWORD || "";
+let credentials = "";
+if (username && password) {
+  credentials = `${username}:${password}@`;
+}
+const mongoUrl = process.env.MONGO_URL || "localhost:27017/livebin";
+
+const db = ShareDBMongo(`mongodb://${credentials}${mongoUrl}`);
 
 ShareDB.types.register(type);
 
 const app = express();
-const backend = new ShareDB();
+const backend = new ShareDB({db});
 
 const server = http.createServer(app);
 
